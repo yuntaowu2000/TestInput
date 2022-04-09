ef434d04bd9811d6a6c6ad30d21e2ab43f5f394b
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2022-04-08 17:39:17 -0700

    screen rotation with camera

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] GameObject screen = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // simulate the rotation of the head camera
+        float horizontalSpeed = Input.GetAxis("Mouse X");
+        transform.RotateAround(transform.position, transform.up, horizontalSpeed * rotationSpeed);
+    }
+
+    void LateUpdate() {
+        if (screen == null) return;
+        // set the screen location to be in front of the user
+        Vector3 newScreenLocation = transform.position + transform.forward * 150f;
+        screen.transform.position = newScreenLocation;
+        // set the screen rotation, 
+        // so that the screen always faces the user at the correct direction.
+        screen.transform.LookAt(this.transform.position);
+        screen.transform.forward = -screen.transform.forward;
+    }
+}
